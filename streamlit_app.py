25fc2f09a047b00df4edbab99a3b242a6cd3238d
annie-ye4 <113400571+annie-ye4@users.noreply.github.com>
2024-07-17 21:57:26 -0400

    first change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("countdown until i can see sid again 🔥")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
