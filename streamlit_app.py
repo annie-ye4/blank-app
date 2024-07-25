133527ab971af5ef66343654e1748918d20bdff9
annie-ye4 <113400571+annie-ye4@users.noreply.github.com>
2024-07-24 22:34:07 -0400

    button change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from datetime import datetime, timedelta
 import time
-from datetime import timedelta
 import pytz
 
 st.set_page_config(page_title="siddy countdown", page_icon=":smiley:")
@@ -9,7 +8,6 @@ st.set_page_config(page_title="siddy countdown", page_icon=":smiley:")
 # Set the target date for the countdown
 ny_tz = pytz.timezone("America/New_York")
 target_date = ny_tz.localize(datetime(2024, 7, 25, 22, 30, 0, 0))
-
 
 def get_time_remaining(target_date):
     # Ensure now is timezone-aware in the same timezone as target_date
@@ -70,7 +68,6 @@ st.markdown('<h1 style="text-align: center;">days until i see siddy again 🔥</
 placeholder = st.empty()
 
 with st.container() as main_container:
-
     st.markdown('<div class="center">', unsafe_allow_html=True)
     st.markdown('<div class="image-container">', unsafe_allow_html=True)
     st.image("photo.png", width=800, use_column_width=True)
@@ -81,7 +78,10 @@ while True:
     days, hours, minutes, seconds = get_time_remaining(target_date)
     with placeholder.container():
         st.markdown('<p class="big-font">time until august 26</p>', unsafe_allow_html=True)
-        st.markdown(f"<div class='countdown-box'>{days} days, {hours}h, {minutes}m, {seconds}s</div>", unsafe_allow_html=True)
+        if days > 0:
+            st.markdown(f"<div class='countdown-box'>{days} days, {hours}h, {minutes}m, {seconds}s</div>", unsafe_allow_html=True)
+        else:
+            st.markdown(f"<div class='countdown-box'>{hours}h, {minutes}m, {seconds}s</div>", unsafe_allow_html=True)
     time.sleep(1)
 
 st.title("bye sid.")
